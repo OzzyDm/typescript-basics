974f0827938987740ab3a4979ca4f8352ca08f40
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-05-19 15:50:53 -0400

    working with enums

--- app.ts
+++ app.ts
@@ -1,17 +1,25 @@
 // const person: {
-//     name: string,
-//     age: number
-// } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// }
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Oguzhan",
   age: 23,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 //person.role.push("admin"); this works!
@@ -27,4 +35,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //console.log(hobby.map()); !!! ERROR !!!
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
