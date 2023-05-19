b98955d74d9c881a8ab04ab0bcfbcc9468662772
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-05-19 15:41:57 -0400

    working with tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //     name: string,
 //     age: number
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Oguzhan",
   age: 23,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+//person.role.push("admin"); this works!
+
+//person.role[1] = 10; error!
+//person.role = [0, 'admin', 'user'] error!
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
